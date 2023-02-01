9e5e86f89d9e95a9fd7101f842127bd553b83a4f
ehe <erhe@health.ucsd.edu>
2023-02-01 11:14:49 -0500

    plotted control data

--- df_handling.R
+++ df_handling.R
@@ -1,4 +1,7 @@
-#Percent Change from Control
+#Load in necessary packages 
+library(tidyverse)
+
+#Load in dataset from iOS
 murine_efflux_data <- read_excel("~/Library/Mobile Documents/com~apple~CloudDocs/UNC PKPD/effluxtransporter/data/murine_efflux_data.xlsx", 
                                  sheet = "df", col_types = c("text", "text", 
                                                              "numeric", "numeric", "numeric", 
@@ -15,3 +18,26 @@ murine_efflux_data<- murine_efflux_data %>%
 #calculate % change from control 
 murine_efflux_data <- murine_efflux_data %>% 
         mutate(PCHG = (((logCFU_mL-1.5)/logCFU_mL)*100)) #1.5 is average logCFU for control 
+#plot mean for each time point with standard errors
+ggplot(data=murine_efflux_data,
+       aes(x=Time_hrs,y=avgCFU,color=Treatment))+
+        geom_point(size=2)+
+        geom_line()+
+        geom_errorbar(aes(ymin=avgCFU-var,ymax=avgCFU+var,width=0.2))+
+        ggtitle("Bacterial Burden over Time across all Treatments")+
+        scale_x_continuous(breaks=c(-8,0,2,8,24))+
+        scale_y_continuous(breaks=c(0,1,2,3),limits=c(0,3))+
+        xlab("Time Sampled (hours)")+
+        ylab("Log CFU/mL")+
+        theme_bw()
+#Percent Change (negative y-axis) 
+ggplot(data=murine_efflux_data,
+       aes(x=Time_hrs,y=PCHG,color=Treatment))+
+        geom_point()+
+        geom_line()+
+        ggtitle("% Change from Baseline for Bacterial Burden over Time across all Treatments")+
+        scale_x_continuous(breaks=c(2,8,24))+
+        xlab("Time Sampled (hours)")+
+        ylab("% Change from Control")+
+        theme_bw()+
+        theme(legend.position="none")#removes legend 
